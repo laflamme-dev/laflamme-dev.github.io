31ba38caacc61583c72ebe7a9bf09abb278ba088
laflamme-dev <39426022+laflamme-dev@users.noreply.github.com>
2021-02-21 14:46:20 -0500

    Add files via upload

--- mlgl.js
+++ mlgl.js
@@ -1,16 +1,25 @@
-
-
-
 function d100()
 {return Math.floor(Math.random() * 100) + 1}; // returns a random integer from 1 to 100
 
-shoot = prompt("quel est ton score (pourcentage) pour les armes automatiques")
-shoot = Number(shoot)
-for (i=0;i<=5;i++) {
+
+
+shooting = prompt("quel est ton score (pourcentage) pour les armes automatiques (enter pour 50)")
+if (shooting =="") {shooting=50}
+shooting = Number(shooting)
+
+
+	
+	
+function MyFunction() {
+	document.write("<br> Fire !!")
+	for (i=0;i<=5;i++) {
 	
 	roll=d100()
-	if (roll<shoot){result ="you hit"} else {result = "you miss"}
+	//result=""
+	result = "Your skill is "+String(shooting)+". " 
+	if (roll<shooting){result =result+"You hit with "+String(roll)} else {result = result+"You miss with "+String(roll)}
+	document.write("<br>")
 	
 	document.write("<br> Bonjour Gab voici du roll de pourcentage <br>" + result)
 	}
-
+	}
